f00ac6e73645c8a129b75811acabee31d5d8819a
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2018-06-13 15:35:42 +0100

    Remove FOUC for Radio conditional reveals
    Uses the presence of the `js-enabled` class to know when to hide or show content.

--- src/components/radios/radios.js
+++ src/components/radios/radios.js
@@ -40,7 +40,7 @@ Radios.prototype.setAttributes = function ($input) {
   $input.setAttribute('aria-expanded', inputIsChecked)
 
   var $content = document.querySelector('#' + $input.getAttribute('aria-controls'))
-  $content.setAttribute('aria-hidden', !inputIsChecked)
+  $content.classList.toggle('js-hidden', !inputIsChecked)
 }
 
 Radios.prototype.handleClick = function (event) {
